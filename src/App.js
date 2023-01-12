d47cdd76b56cf267b34178300bc8b35c8fb2ff72
Michelle Ross <miross629@gmail.com>
2023-01-11 17:17:38 -0800

    Build App.js component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import React from 'react';
+
+import Form from './components/Form';
+import Stats from './components/Stats';
+import Character from './components/Character';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="App">
+      <h1>Character Designer</h1>
+      <section className="container">
+        <div className="left">
+          <Form />
+          <Stats />
+        </div>
+        <div className="right">
+          <Character />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
