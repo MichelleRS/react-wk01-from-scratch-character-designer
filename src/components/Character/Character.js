8613d716c688979cce00989405c7e27ff132d1b0
Michelle Ross <miross629@gmail.com>
2023-01-12 13:23:02 -0800

    Edit css for Character images

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -5,15 +5,9 @@ export default function Character({ head, shirt }) {
   return (
     <div className="character">
       {/* head */}
-      <div
-        className="head-image"
-        style={{ backgroundImage: `url(./head-${head}.png)`, height: '500px' }}
-      ></div>
+      <div className="head-image" style={{ backgroundImage: `url(./head-${head}.png)` }}></div>
       {/* shirt */}
-      <div
-        className="shirt-image"
-        style={{ backgroundImage: `url(./shirt-${shirt}.png)`, height: '500px' }}
-      ></div>
+      <div className="shirt-image" style={{ backgroundImage: `url(./shirt-${shirt}.png)` }}></div>
       {/* pants */}
     </div>
   );
