7f802488e97933d2d526300546a3733e184bf6b3
Michelle Ross <miross629@gmail.com>
2023-01-13 10:36:32 -0800

    Style Stats and App container

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Stats.css';
 
 export default function Stats({ headCount, shirtCount, pantsCount, catchphrases }) {
   return (
-    <div>
+    <div className="stats">
       <p>
         You have changed the head {headCount} times, shirt {shirtCount} times, and pants{' '}
         {pantsCount} times.
       </p>
+      <p>Your character catphrases:</p>
       <ul className="catchphrases">
         {catchphrases.map((catchphrase) => (
           <li className="catchphrase" key={catchphrase}>
